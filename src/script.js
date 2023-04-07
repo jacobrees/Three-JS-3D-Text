e2e0b10b0a79cc1368f68fbe53d4ce2bfa043b01
jacobrees <jacob.rees97@yahoo.com>
2023-04-07 22:52:45 +0100

    Complete project

--- src/script.js
+++ src/script.js
@@ -47,6 +47,24 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
   const textMaterial = new THREE.MeshMatcapMaterial({matcap: matcapTexture});
   const text = new THREE.Mesh(textGeometry, textMaterial);
   scene.add(text);
+
+  const donutGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45)
+  const donutMaterial = new THREE.MeshMatcapMaterial({matcap: matcapTexture});
+
+  for(let i = 0; i < 100; i++) {
+    const donut = new THREE.Mesh(donutGeometry, donutMaterial);
+    donut.position.x = (Math.random() - 0.5) * 10
+    donut.position.y = (Math.random() - 0.5) * 10
+    donut.position.z = (Math.random() - 0.5) * 10
+
+    donut.rotation.x = Math.random() * Math.PI
+    donut.rotation.y = Math.random() * Math.PI
+
+    const scale = Math.random()
+    donut.scale.set(scale, scale, scale)
+
+    scene.add(donut);
+  }
 });
 
 /**
